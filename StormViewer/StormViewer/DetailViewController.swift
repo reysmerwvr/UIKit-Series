15e5adb6b39348aeb894e9e4b29c7d441befa47b
Reysmer Valle <reysmerwvr@gmail.com>
2021-03-29 20:06:54 -0500

    Add bar button to share image

--- StormViewer/StormViewer/DetailViewController.swift
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -18,6 +18,8 @@ class DetailViewController: UIViewController {
         
         title = imagePosition // title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -32,6 +34,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image, selectedImage!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 
